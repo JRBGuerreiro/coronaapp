248b58941f47c7a1954c9297ef905cc69bffaad2
JorgeGuerreiro <crcguerreiro@gmail.com>
2021-01-15 17:10:53 +0000

    wip

--- corona-app/src/components/Map.js
+++ corona-app/src/components/Map.js
@@ -8,6 +8,8 @@ const Map = React.memo(({data}) => {
 
     const mapRef = useRef(null)
 
+	const [loading, setLoading] = useState(true)
+
     useEffect(() => {
         const map = new mapboxgl.Map({
             container: mapRef.current,
@@ -24,7 +26,13 @@ const Map = React.memo(({data}) => {
           // add navigation control (the +/- zoom buttons)
         map.addControl(new mapboxgl.NavigationControl(), 'bottom-right');
 
-
+		const loadingScreen = document.querySelector(".loader-container")
+		setTimeout(() => {
+			if(loadingScreen && loading){
+				loadingScreen.remove()
+				setLoading(false)
+			}	
+		}, 4000);
         return () => {
             //cleanup when unmounting
             map.remove()
@@ -39,40 +47,65 @@ const Map = React.memo(({data}) => {
     const [active, setActive] = useState(0)
 
     const addMarkerToMap = async (mapData, map) =>  {
-        if(Object.keys(mapData).length) {
-            mapData.forEach(el => {
-                const {countryInfo, country, cases, population, recovered, deaths, active} = el
-                const {lat, long, flag} = countryInfo
-                console.log(el)
-                const marker = new mapboxgl.Marker()
-                .setLngLat([long, lat])
-                
-                .addTo(map)
+        map.on('load', function() {
+            map.addLayer(
+              {
+                id: 'country-boundaries',
+                source: {
+                  type: 'vector',
+                  url: 'mapbox://mapbox.country-boundaries-v1',
+                },
+                'source-layer': 'country_boundaries',
+                type: 'fill',
+                paint: {
+                  'fill-color': '#3fb1ce',
+				  'fill-opacity': 0.4,
+                },
+              },
+              'country-label'
+            );
+        });
 
-                var popup = new mapboxgl.Popup()
-                marker.getElement().addEventListener('click', () => {
-                    setPopulation(population)
-                    setInfected(cases)
-                    setRecovered(recovered)
-                    setDeaths(deaths)
-                    setActive(active)
-                })
+		/**
+		 * Here we pick pick up events for each polygon (country)
+		 * Once we do we grab data from the api and trigger the click event for the country with the same iso code
+		 */
+        map.on('click', (event) => {
+            if(Object.keys(mapData).length) {
+              	var features= map.queryRenderedFeatures(event.point, {layers: ['country-boundaries']})
+              	if(!features.length) {
+                	return;
+				}
+				  
+				mapData.forEach(el => {
+					var feature = features[0]
+					const {countryInfo, country, cases, population, recovered, deaths, active} = el
+					const {lat, long, flag, iso2} = countryInfo
+					if(feature.properties.iso_3166_1 === iso2) {
+						var popup = new mapboxgl.Popup()
+						popup
+							.setLngLat([long, lat])
+							.setHTML(
+								`<img style="width: 100%" src=${flag} alt="flag"/> 
+								<p style="text-align:center; color: #fcfcfc">${country}</p>
+							`)
+							.addTo(map)
 
-                marker.getElement().addEventListener('mouseenter', () => {
-                    popup
-                    .setLngLat([long, lat])
-                    .setHTML(
-                        `<img style="width: 100%" src=${flag} alt="flag"/> 
-                        <h1 style="text-align:center; color: #fcfcfc">${country}</h1>
-                    `)
-                    .addTo(map)
-                })
-
-                marker.getElement().addEventListener("mouseleave", () => {
-                    popup.remove()
-                })
-            })
-        }
+						setPopulation(population)
+						setInfected(cases)
+						setRecovered(recovered)
+						setDeaths(deaths)
+						setActive(active)
+					}
+				})
+			}
+		})
+		
+		///make sure we signal the user this is a clickable polygon
+		// map.on('mousemove', (event) => {
+		// 	var features= map.queryRenderedFeatures(event.point, {layers: ['country-boundaries']})
+		// 	map.getCanvas().style.cursor = (features.length) ? 'pointer' : ''
+		// })
     }
 
     return(
